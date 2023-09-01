10942d720b92853915dd17f40867630171116f71
Chinmay Singhal <chinmay.singhal08@gmail.com>
2023-09-01 20:13:25 +0530

    add firestore for storing todos

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import { auth } from "./firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import TodoApp from "./pages/TodoApp";
 import Welcome from "./pages/Welcome";
+import TodoProvider from "./context/TodoContext.jsx";
 
 function App() {
   const [user] = useAuthState(auth);
 
-  return <div className="App">{user ? <TodoApp /> : <Welcome />}</div>;
+  return (
+    <TodoProvider>
+      <div className="App">{user ? <TodoApp /> : <Welcome />}</div>
+    </TodoProvider>
+  );
 }
 
 export default App;
--- src/context/TodoContext.jsx
+++ src/context/TodoContext.jsx
@@ -1,19 +1,69 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
+import {
+  query,
+  collection,
+  doc,
+  setDoc,
+  updateDoc,
+  getDocs,
+  deleteDoc,
+  where,
+} from "firebase/firestore";
+import { auth, db } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const TodoContext = createContext();
 
 export const useTodoContext = () => useContext(TodoContext);
 
 const TodoProvider = ({ children }) => {
+  const [user, loading, error] = useAuthState(auth);
+
+  const collectionRef = collection(db, "todos");
   const [todos, setTodo] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!loading && !error && user) {
+        const q = query(collection(db, "todos"), where("user", "==", user.uid));
+        try {
+          const snapshot = await getDocs(q);
+          const todosData = snapshot.docs.map((doc) => ({
+            key: doc.id,
+            ...doc.data(),
+          }));
+          setTodo(todosData);
+        } catch (fetchError) {
+          console.error("Error fetching todos:", fetchError);
+        }
+      }
+    };
+
+    // Execute fetchData when user is available and there are no errors
+    fetchData();
+  }, [user, loading, error]);
+
   const addTodo = (value) => {
-    // ! Don't use the length of the array for key
-    setTodo([...todos, { value: value, isDone: false, key: todos.length + 1 }]);
+    const todoRef = doc(collectionRef);
+    setDoc(todoRef, {
+      user: user.uid,
+      value: value,
+      isDone: false,
+    }).then(console.log("Added todo"));
+    setTodo([
+      ...todos,
+      {
+        value: value,
+        isDone: false,
+        key: todoRef.id,
+        user: user.uid,
+      },
+    ]);
   };
 
   const deleteTodo = (key) => {
     const updatedTodos = todos.filter((todo) => todo.key !== key);
+    deleteDoc(doc(db, "todos", key)).then(console.log("Deleted todo"));
     setTodo(updatedTodos);
   };
 
@@ -22,8 +72,14 @@ const TodoProvider = ({ children }) => {
       if (todo.key === key) {
         // Update the value if it is provided else update the todo completion status
         if (value) {
+          updateDoc(doc(db, "todos", key), {
+            value: value,
+          }).then(console.log("Updated todo value"));
           todo.value = value;
         } else {
+          updateDoc(doc(db, "todos", key), {
+            isDone: !todo.isDone,
+          }).then(console.log("Updated todo status"));
           todo.isDone = !todo.isDone;
         }
       }
